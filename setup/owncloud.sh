844c7fb97ce6576fdd6aef9f1727b7cf5e3b0f1d
Frank Lemanschik <frank@github.dspeed.eu>
2017-09-14 12:56:07 +0100

    Update owncloud.sh

--- setup/owncloud.sh
+++ setup/owncloud.sh
@@ -9,6 +9,9 @@ source /etc/mailinabox.conf # load global vars
 
 echo "Installing Nextcloud (contacts/calendar)..."
 
+# Install stuff
+apt-get -y install mysql-server mysql-client
+
 # Keep the php5 dependancies for the owncloud upgrades
 apt_install \
 	dbconfig-common \
@@ -19,7 +22,9 @@ apt-get purge -qq -y owncloud*
 
 apt_install php7.0 php7.0-fpm \
 	php7.0-cli php7.0-sqlite php7.0-gd php7.0-imap php7.0-curl php-pear php-apc curl \
-        php7.0-dev php7.0-gd memcached php7.0-memcached php7.0-xml php7.0-mbstring php7.0-zip php7.0-apcu
+        php7.0-dev php7.0-gd memcached php7.0-memcached php7.0-xml php7.0-mbstring php7.0-zip php7.0-apcu \
+        php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache\
+        php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php-gettext
 
 # Migrate <= v0.10 setups that stored the ownCloud config.php in /usr/local rather than
 # in STORAGE_ROOT. Move the file to STORAGE_ROOT.
