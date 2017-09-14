4653efc84958d07fc59ea8320e698897ec668139
Frank Lemanschik <frank@github.dspeed.eu>
2017-09-14 13:00:15 +0100

    Create php7.sh

---
+++ setup/php7.sh
@@ -0,0 +1,6 @@
+# Install PHP7-FPM
+apt_install php7.0 php7.0-fpm \
+	php7.0-cli php7.0-sqlite php7.0-gd php7.0-imap php7.0-curl php-pear php-apc curl \
+  php7.0-dev php7.0-gd memcached php7.0-memcached php7.0-xml php7.0-mbstring php7.0-zip php7.0-apcu \
+  php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache\
+  php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php-gettext
