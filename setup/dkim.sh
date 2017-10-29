1f0ea7c8d9c80f0b4a38d7893fb9d00560a8ba93
Frank Lemanschik <frank@dspeed.eu>
2017-10-29 22:14:21 +0000

    Fixed Ubuntu Version Warning

--- setup/dkim.sh
+++ setup/dkim.sh
@@ -76,6 +76,7 @@ tools/editconf.py /etc/postfix/main.cf \
 	milter_default_action=accept
 
 # Restart services.
-restart_service opendkim
-restart_service opendmarc
-restart_service postfix
+## DNS upgrade needs to run to write inital table
+#restart_service opendkim
+#restart_service opendmarc
+#restart_service postfix
