72ff2b8908adedfa9041d09088950c1997d9cb91
Frank Lemanschik <github+frank-dspeed@lemanschik.com>
2024-04-10 07:37:57 +0200

    Update web_update.py
    remove none needed templates

--- management/web_update.py
+++ management/web_update.py
@@ -85,12 +85,9 @@ def do_web_update(env):
 
 	# Load the templates.
 	template0 = read_conf("nginx.conf")
-	template1 = read_conf("nginx-alldomains.conf")
-	template2 = read_conf("nginx-primaryonly.conf")
-	template3 = "\trewrite ^(.*) https://$REDIRECT_DOMAIN$1 permanent;\n"
 
 	# Add the PRIMARY_HOST configuration first so it becomes nginx's default server.
-	nginx_conf += make_domain_config(env['PRIMARY_HOSTNAME'], [template0, template1, template2], ssl_certificates, env)
+	nginx_conf += make_domain_config(env['PRIMARY_HOSTNAME'], [template0, "\tinclude /etc/nginx/snippets/miab-primaryonly.conf\n"], ssl_certificates, env)
 
 	# Add configuration all other web domains.
 	has_root_proxy_or_redirect = get_web_domains_with_root_overrides(env)
@@ -102,12 +99,12 @@ def do_web_update(env):
 		if domain in web_domains_not_redirect:
 			# This is a regular domain.
 			if domain not in has_root_proxy_or_redirect:
-				nginx_conf += make_domain_config(domain, [template0, template1], ssl_certificates, env)
+				nginx_conf += make_domain_config(domain, [template0], ssl_certificates, env)
 			else:
 				nginx_conf += make_domain_config(domain, [template0], ssl_certificates, env)
 		else:
 			# Add default 'www.' redirect.
-			nginx_conf += make_domain_config(domain, [template0, template3], ssl_certificates, env)
+			nginx_conf += make_domain_config(domain, [template0, "\trewrite ^(.*) https://$REDIRECT_DOMAIN$1 permanent;\n"], ssl_certificates, env)
 
 	# Did the file change? If not, don't bother writing & restarting nginx.
 	nginx_conf_fn = "/etc/nginx/local.conf"
